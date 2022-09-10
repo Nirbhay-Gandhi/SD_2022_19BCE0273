f7e2e78e55ac11b8ece956cc090d3a3dc5d814d4
Nirbhay-Gandhi <98534533+Nirbhay-Gandhi@users.noreply.github.com>
2022-09-10 19:48:20 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,13 +6,15 @@ class global_chess
 {
 public:
     string board[5][5];
+    string status;
 public:
     global_chess()
     {
         cout<<"***Player A to be at bottom and Player B to be at top***"<<endl;
+        status="e"; //e means empty
         for(int i=0;i<5;i++){
             for(int j=0;j<5;j++){
-                this->board[i][j]="e";
+                this->board[i][j]=status;
             }
         }
     }
@@ -22,7 +24,7 @@ public:
         for(int i=0;i<5;i++){
                 cout<<"\t";
             for(int j=0;j<5;j++){
-                if(board[i][j]=="e"){
+                if(status=="e"){
                     cout<<"-"<<"  ";
                 }else{
                     cout<<board[i][j]<<" ";
@@ -34,12 +36,59 @@ public:
     }
 };
 
-class participent : public global_chess
+class avatars : public global_chess
+{
+public:
+    vector<pair<string,string>> input_char1(5); //A
+    vector<pair<string,string>> input_char2(5); //B
+    string peice;
+public:
+    int get_index(string element)
+    {
+        int current_pos=0;
+        for(int k=0;k<5;k++){
+            if(input_char1[k]==character){
+                current_pos=k;
+                break;
+            }
+        }
+        return current_pos;
+    }
+    void movement(char player, string my_character, char next) //(A,P2,F)
+    {
+        if(player=='A')
+        {
+            //[4][i]
+            if(next=='B'){
+                cout<<"Error02: Cannot move Backward further!"<<endl;
+            }
+            else{
+                    peice=my_character[0];
+                    int current_row=4;
+                    int current_col=get_index(my_character); //we get i
+                    if(next=='F')//row same col down
+                    {
+                        string temp=board[current_row][current_col];
+                        current_row--;
+                        board[]
+                    }
+            }
+        }
+        if(player=='B')
+        {
+            
+        }
+        else{
+            cout<<"Error01: Player name should be either A or B."<<endl;
+        }
+    }
+};
+
+class participent : public global_chess, public avatars
 {
 private:
     char id1;
     char id2;
-    string input_char[5];
 public:
     participent(char id1,char id2)
     {
@@ -53,16 +102,27 @@ public:
     void input(char p)
     {
         cout<<"Player-"<<p<<": ";
-        for(int i=0;i<5;i++){
-            cin>>input_char[i];
-        }
         if(p=='B'){
             for(int i=0;i<5;i++){
-                board[0][i]=input_char[i];
+                string temp,tindex="";
+                cin>>temp;
+                tindex+="0";
+                tindex+=to_string(i);
+                input_char2.push_back(make_pair(temp,tindex));
+            }
+            for(int i=0;i<5;i++){
+                board[0][i]=input_char2[i].first;
             }
         }else if(p=='A'){
             for(int i=0;i<5;i++){
-                board[4][i]=input_char[i];
+                string temp,tindex="";
+                cin>>temp;
+                tindex+="0";
+                tindex+=to_string(i);
+                input_char2.push_back(make_pair(temp,tindex));
+            }
+            for(int i=0;i<5;i++){
+                board[4][i]=input_char1[i]first;
             }
         }
 
