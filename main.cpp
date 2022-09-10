7dda27814a137e59c1b9e35373ba4e91e30520ca
Nirbhay-Gandhi <98534533+Nirbhay-Gandhi@users.noreply.github.com>
2022-09-10 18:57:46 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,42 +23,57 @@ public:
                 cout<<"\t";
             for(int j=0;j<5;j++){
                 if(board[i][j]=="e"){
-                    cout<<"-"<<" ";
+                    cout<<"-"<<"  ";
+                }else{
+                    cout<<board[i][j]<<" ";
                 }
             }
             cout<<endl;
         }
+        cout<<endl;
     }
 };
 
 class participent : public global_chess
 {
 private:
-    char id;
+    char id1;
+    char id2;
     string input_char[5];
 public:
-    participent(char id)
+    participent(char id1,char id2)
     {
-        if(id=='A' || id=='B')
-            this->id=id;
+        if(id1=='A' || id1=='B' && id2=='A' || id2=='B'){
+            this->id1=id1;
+            this->id2=id2;
+        }
         else
             cout<<"Error01: Player name should be either A or B."<<endl;
     }
-    void input()
+    void input(char p)
     {
-        cout<<"Player-"<<id<<": ";
+        cout<<"Player-"<<p<<": ";
         for(int i=0;i<5;i++){
             cin>>input_char[i];
         }
-
-    }
-    void initial_propogate(char p_id)
-    {
-        if(p_id=='A'){
+        if(p=='B'){
             for(int i=0;i<5;i++){
                 board[0][i]=input_char[i];
             }
-        }else if(p_id=='B'){
+        }else if(p=='A'){
+            for(int i=0;i<5;i++){
+                board[4][i]=input_char[i];
+            }
+        }
+
+    }
+    /*void initial_propogate(char p_id)
+    {
+        if(p_id=='B'){
+            for(int i=0;i<5;i++){
+                board[0][i]=input_char[i];
+            }
+        }else if(p_id=='A'){
             for(int i=0;i<5;i++){
                 board[4][i]=input_char[i];
             }
@@ -66,7 +81,7 @@ public:
         else{
             cout<<"Error01: Player name should be either A or B."<<endl;
         }
-    }
+    }*/
 };
 
 int main()
@@ -75,7 +90,17 @@ int main()
     cout<<"||Student name:        Nirbhay S Gandhi                               ||"<<endl;
     cout<<"||Reg. No:             19BCE0273        (VIT-Vellore)                 ||\n"<<endl;
 
+    /**Initializing the game*/
     global_chess board1;
     board1.display();
+
+    //player-A
+    participent p('A','B');
+    p.input('A');
+    p.display();
+    p.input('B');
+    p.display();
+
+
     return 0;
 }
